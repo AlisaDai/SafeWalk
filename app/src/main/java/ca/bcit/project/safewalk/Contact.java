02825f3c71e5aec506484116bfad73aefcf3c372
AlisaDai <alisa1994@msn.cn>
2018-11-01 10:54:10 -0700

    Add Contact Page

---
+++ app/src/main/java/ca/bcit/project/safewalk/Contact.java
@@ -0,0 +1,29 @@
+package ca.bcit.project.safewalk;
+
+public class Contact {
+    private String _type;
+    private String _name;
+    private String _phoneNumber;
+
+    public static final Contact[] phoneCall = {
+            new Contact("EMERGENCY", "POLICE（EMERGENCY）", "911"),
+            new Contact("NON-EMERGENCY", "POLICE（NON-EMERGENCY）", "6045255411"),
+    };
+
+    public Contact(String type, String name, String phoneNumber) {
+        _type = type;
+        _name = name;
+        _phoneNumber = phoneNumber;
+    }
+
+    public String getType() {
+        return _type;
+    }
+    public String getName() {
+        return _name;
+    }
+    public String getPhoneNumber() { return _phoneNumber; }
+    public String toString() {
+        return _name;
+    }
+}
--- app/src/main/java/ca/bcit/project/safewalk/ContactActivity.java
+++ app/src/main/java/ca/bcit/project/safewalk/ContactActivity.java
@@ -1,11 +1,16 @@
 package ca.bcit.project.safewalk;
 
 import android.content.Intent;
+import android.net.Uri;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
+import android.view.View;
+import android.widget.AdapterView;
+import android.widget.ArrayAdapter;
+import android.widget.ListView;
 
 public class ContactActivity extends AppCompatActivity {
 
@@ -16,6 +21,25 @@ public class ContactActivity extends AppCompatActivity {
 
         Toolbar toolbar = findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        ArrayAdapter<Contact> arrayAdapter = new ArrayAdapter<>(
+                this, android.R.layout.simple_list_item_1, Contact.phoneCall
+        );
+
+        ListView listContacts = findViewById(R.id.contactList);
+        listContacts.setAdapter(arrayAdapter);
+
+
+        listContacts.setOnItemClickListener(new AdapterView.OnItemClickListener() {
+            @Override
+            public void onItemClick(AdapterView<?> l, View v, int position, long id) {
+                //Intent intent = new Intent(getApplicationContext(), ContactDetailsActivity.class);
+                //intent.putExtra("index", (int)id);
+                //startActivity(intent);
+                String phone = Contact.phoneCall[(int)id].getPhoneNumber();
+                call(phone);
+            }
+        });
     }
 
     @Override
@@ -31,16 +55,22 @@ public class ContactActivity extends AppCompatActivity {
         switch (item.getItemId()) {
             case R.id.action_map:
                 //Toast.makeText(this, "Map", Toast.LENGTH_SHORT).show();
-                //Intent i = new Intent(this, MainActivity.class);
+                //Intent j = new Intent(this, ContactActivity.class);
                 startActivity(new Intent(this, MainActivity.class));
                 return true;
             case R.id.action_news:
-                //Toast.makeText(this, "News",  Toast.LENGTH_SHORT).show();
-                //Intent j = new Intent(this, NewsActivity.class);
+                //Toast.makeText(this, "News", Toast.LENGTH_SHORT).show();
+                //Intent i = new Intent(this, NewsActivity.class);
                 startActivity(new Intent(this, NewsActivity.class));
                 return true;
             default:
                 return super.onOptionsItemSelected(item);
         }
+    }
+
+    private void call(String phone) {
+        Intent intent = new Intent(Intent.ACTION_DIAL, Uri.parse("tel:" + phone));
+        intent.setFlags(Intent.FLAG_ACTIVITY_NEW_TASK);
+        startActivity(intent);
     }
 }
